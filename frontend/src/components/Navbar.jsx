554dc01d68f736385bcd5d489191eafa11d06c3e
Trong Nhan <nhantrinh2005blmn@gmail.com>
2025-05-28 21:37:25 +0700

    navbar updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,6 +7,8 @@ function NavBar() {
   const {pathname} = useResolvedPath()
   const isHomePage = pathname === '/'
 
+  const {products} = useProductStore();
+
   return <div className="bg-base-100/80 backdrop-blur-lg border-b border-base-conten/10 sticky top-0 z-50">
     <div className="max-w-7xl mx-auto">
       <div className="navbar px-4 min-h-[4rem] justify-between">
@@ -34,7 +36,7 @@ function NavBar() {
               <div className="indicator">
                 <div className="p-2 rounded-full hover:bg-base-200 transition-colors">
                   <ShoppingBagIcon className="size-5" />
-                  <span className="badge badge-sm badge-primary indicator-item">8</span>
+                  <span className="badge badge-sm badge-primary indicator-item">{products.length}</span>
                 </div>
 
               </div>
